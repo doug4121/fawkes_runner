00bc3f15d3338cb649460ffa13604110a0884799
Douglas Chang <doug4121@gmail.com>
2025-01-22 03:30:07 -0800

    created a runner

---
+++ runner.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.path.insert(1, '/fawkes')
+
+from fawkes.protection import Fawkes
+import os
+
+sys.argv[1]
+
+image_paths = {sys.argv[1]}
+my_fawkes = Fawkes("extractor_2", '0', 1)
+my_fawkes.run_protection(image_paths, debug=True)
