77b993f925e99cbf75c6b4e8c6e1b3249e0400f9
Douglas Chang <doug4121@gmail.com>
2025-01-24 00:46:02 -0800

    Update runner.py
    have a comment to run off normal fawkes repo. now running high cloaking

--- runner.py
+++ runner.py
@@ -1,10 +1,11 @@
 import sys
 
 sys.path.insert(1, '/personal_fawkes')
+#sys.path.insert(1, '/fawkes')
 
 from fawkes.protection import Fawkes
 import os
 
 image_paths = {sys.argv[1]}
-my_fawkes = Fawkes("extractor_2", '0', 1)
+my_fawkes = Fawkes("extractor_2", '0', 1, mode="high")
 my_fawkes.run_protection(image_paths, debug=True)
